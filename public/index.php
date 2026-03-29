6dbb529c5802260b100fa0794b11ae13b0e8793a
Tiago França <github@tiagofranca.com>
2026-03-29 05:53:45 -0300

    try uri fix
    	modified:   public/index.php

--- public/index.php
+++ public/index.php
@@ -10,6 +10,10 @@ if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php'
     require $maintenance;
 }
 
+$uri = $_SERVER['REQUEST_URI'] ?? '/';
+
+$_SERVER['REQUEST_URI'] = str_starts_with($uri, '/api/api') ? substr($uri, 8) : $uri;
+
 // Register the Composer autoloader...
 require __DIR__ . '/../vendor/autoload.php';
 
