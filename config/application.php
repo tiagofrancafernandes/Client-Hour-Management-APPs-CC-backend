2d41cafe15542e5c6fb0fcc095db4695c16d221d
Tiago França <github@tiagofranca.com>
2026-03-30 16:33:42 -0300

    fix: update application configuration for self-registration and password recovery
    - Add configuration for `self_recovery_password` and `self_register`
    - Update default roles for self-registration and user creation
    
    Changes to be committed:
    	modified:   config/application.php

--- config/application.php
+++ config/application.php
@@ -1,12 +1,45 @@
 <?php
 
+$disabledResources = (array) value(function () {
+    $values = array_values(
+        array_filter(array_map('trim', explode(';', ensure_string(env('DISABLED_RESOURCES')))))
+    );
+
+    return $values;
+});
+
+$availabledResources = (array) value(function () use ($disabledResources) {
+    $defaultAvailable = array_keys(array_filter([
+        // 'abc' => true,
+        // 'def' => false,
+        // 'ghi' => true,
+    ]));
+
+    $available = array_merge(array_values(
+        array_filter(array_map('trim', explode(';', ensure_string(env('AVAILABLED_RESOURCES')))))
+    ), $defaultAvailable);
+
+    $available = array_filter(array_combine($available, $available));
+
+    return array_diff(array_keys($available), $disabledResources);
+});
+
 return [
-    'enabled_resources' => env('ENABLED_RESOURCES'),
-    'disabled_resources' => env('DISABLED_RESOURCES'),
+    'disabled_resources' => $disabledResources,
+    'availabled_resources' => $availabledResources,
     'resources' => [
         'auth' => [
             /* Show link and form to recovery/renew password */
-            'self_recovery_password' => (bool) env('self_recovery_password'),
+            'self_recovery_password' => in_array('self_recovery_password', $availabledResources),
+
+            /* Show link and form to register */
+            'self_register' => in_array('self_register', $availabledResources),
+
+            /* Default role for user for self registration flow */
+            'default_role_on_register' => null,
+
+            /* Default role for user on creation */
+            'default_role' => null,
         ],
     ],
 ];
